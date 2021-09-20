266ae79fc46dcba91d241d4b56addeb58cb9d217
MichangaVictor <onyango.vic@yandex.com>
2021-09-20 14:59:09 +0300

    authentication and verification

--- accounts/models.py
+++ accounts/models.py
@@ -43,7 +43,7 @@ class Account(AbstractBaseUser):
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=50, unique=False, blank=True)
 
     # required
     date_joined = models.DateTimeField(auto_now_add=True)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'store',
     'carts',
 
-
 ]
 
 MIDDLEWARE = [
@@ -131,3 +130,17 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'onyangovictor096@gmail.com'
+EMAIL_HOST_PASSWORD = 'VictorMichanga@123'
+EMAIL_USE_TLS = True
